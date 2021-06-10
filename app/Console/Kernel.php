8e3333f9397951f3d42cf10502bdd3d639e170bc
Ezequias <ezequias@hotmail.com.br>
2021-06-10 01:42:30 -0500

    Scheduled e-mail

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Mail;
+use App\Jobs\SendHourlyNotification;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+      $schedule->job(new SendHourlyNotification)->hourly();
     }
 
     /**

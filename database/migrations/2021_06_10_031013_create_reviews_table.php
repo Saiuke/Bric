af321cc49f335c2bcfc58f7c0fcdeab1944dc685
Ezequias <ezequias@hotmail.com.br>
2021-06-09 22:48:17 -0500

    Basic routes and migrations

---
+++ database/migrations/2021_06_10_031013_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('stars')->default(0)->comment('Number of stars');
+            $table->string('title')->charset('utf8mb4')->comment('Title of the review');
+            $table->string('review')->charset('utf8mb4')->comment('Body of the review');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
